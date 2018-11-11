5c59a1a766eb363d53f388c765494a8ea6f48df9
Levi Burton <levi.burton@outlook.com>
2018-11-11 00:37:32 -0600

    small change.  testing setup.

--- Assets/_Core/Scripts/Configs/ActionListDb.cs
+++ Assets/_Core/Scripts/Configs/ActionListDb.cs
@@ -5,6 +5,5 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Config/Action DB")]
 public class ActionListDb : ScriptableObject
 {
-    [SerializeField]
     public List<ActionConfig> items = new List<ActionConfig>();
 }
