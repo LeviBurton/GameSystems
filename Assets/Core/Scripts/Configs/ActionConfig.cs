76097d47cd9364939893679f356d95ae1585b275
Levi Burton <levi.burton@outlook.com>
2018-11-14 11:38:04 -0600

    re-structured asset folders.  added a simple sprite preview editor.

---
+++ Assets/Core/Scripts/Configs/ActionConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Config/Action/New Action")]
+public class ActionConfig : ScriptableObject
+{
+    [Header("Properties")]
+    public string actionName;
+    [TextArea(3, 30)] public string Description;
+    [PreviewSprite] public Sprite spriteIcon;
+
+    [Header("Behaviors")]
+    public List<ActionBehavior> behaviors = null;
+
+    public void Execute(ActionSystem system)
+    {
+        foreach (var behavior in behaviors)
+        {
+            behavior.Execute(system, this);
+        }
+    }
+}
+
+
---
+++ Assets/Core/Scripts/Configs/SpellConfig.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Config/Spell/New Spell")]
+public class SpellConfig : ScriptableObject
+{
+    [Header("Properties")]
+    public string spellName;
+    [TextArea(3, 30)] public string Description;
+    [PreviewSprite] public Sprite spriteIcon;
+
+    [Header("Behaviors")]
+    public List<SpellBehavior> behaviors = null;
+
+    [Header("VFX")]
+    public VfxConfig targetVfx = null;
+
+    public void Execute(SpellSystem system)
+    {
+        Transform targetTransform = system.transform;
+
+        var target = system.GetComponent<TargetingSystem>().targetable;
+
+        if (target != null)
+        {
+            targetTransform = target.transform;
+        }
+
+        foreach (var behavior in behaviors)
+        {
+            behavior.Execute(system, this);
+        }
+
+        if (targetVfx != null)
+        {
+            var vfx = Instantiate(targetVfx.vfxPrefab, targetTransform);
+            vfx.GetComponent<VfxSystem>().config = targetVfx;
+        }
+    }
+}
